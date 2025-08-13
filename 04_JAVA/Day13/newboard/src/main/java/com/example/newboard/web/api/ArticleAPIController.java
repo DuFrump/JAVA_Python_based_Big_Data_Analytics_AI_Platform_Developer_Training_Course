04d2a9f9ac88891b971d261fa5a26bcf1d120fc8
DuFrump <jihoon6052@gmail.com>
2025-08-13 13:20:01 +0900

    JAVA: 14일차 오전 업데이트

--- 04_JAVA/Day13/newboard/src/main/java/com/example/newboard/web/api/ArticleAPIController.java
+++ 04_JAVA/Day13/newboard/src/main/java/com/example/newboard/web/api/ArticleAPIController.java
@@ -1,4 +1,37 @@
 package com.example.newboard.web.api;
 
+import com.example.newboard.service.ArticleService;
+import com.example.newboard.web.dto.ArticleCreateRequest;
+import com.example.newboard.web.dto.ArticleUpdateRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.event.spi.DeleteContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/articles")
 public class ArticleAPIController {
+
+    private final ArticleService articleService;
+
+    @PostMapping
+    public ResponseEntity<Void> create(@Valid @RequestBody ArticleCreateRequest req) {
+        articleService.create(req);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id, @Valid @RequestBody ArticleUpdateRequest req) {
+        articleService.update(id, req);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        articleService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
