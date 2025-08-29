f668dee14edc6c4703353acd8bbf73bd2eb2ea28
DuFrump <jihoon6052@gmail.com>
2025-08-29 18:12:50 +0900

    Python: 8일차 못 채운 부분 추가

--- 05_Python/Day08/05_greedy.py
+++ 05_Python/Day08/05_greedy.py
@@ -1,10 +1,34 @@
 import re
 
-print("----- 괄호 안의 내용 뽑기 -----")
+s = '<html><head><title>Title</title>'
 
-text = "오늘의 메뉴는 (자장면)과 (오징어덮밥) 입니다."
+print(len(s))
+
+print(re.match('<.*>', s).span())
+
+print(re.match('<.*>', s).group())
+
+print(re.match('<.*?>', s).group())
+
+# *? +? {m,n}?
+
+text = '1234567 홍길동 1 #$123홍길동'
+pattern = r'\d{2,4}?'
+
+matches = re.findall(pattern, text)
+print(matches)
+print()
+
+print("----- 괄호 안의 내용 뽑기 -----")
+print()
+
+text = '오늘 메뉴는 (자장면) 과 (오징어덮밥) 입니다.'
 
 pattern = r"\((.*?)\)"
 
-menu = re.findall(pattern, text)
-print("메뉴 목록 : ", menu)
+menus = re.findall(pattern, text)
+print(menus)
+
+# ['(자장면)', '(오징어덮밥)']
+
+
