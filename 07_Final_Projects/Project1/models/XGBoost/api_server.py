35f83e2ff026b7e47ac0d701ba9573065912f73e
DuFrump <jihoon6052@gmail.com>
2025-10-23 14:01:34 +0900

    update

--- 07_Final_Projects/Project1/models/XGBoost/api_server.py
+++ 07_Final_Projects/Project1/models/XGBoost/api_server.py
@@ -70,5 +70,5 @@ def predict():
 
 # --- 4. API 서버 실행 ---
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
